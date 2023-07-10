65755619f01b578933e1fa601af09cc17d357785
Will Laws <will.laws@me.com>
2023-07-10 10:01:52 +1000

    adjust frequency to check the alarm status and add State Updated Timestamp

--- lambda-trigger.sh
+++ lambda-trigger.sh
@@ -1,20 +1,40 @@
 #!/bin/bash
 
+# accept an argument that is the name of your lambda function
+# if no argument is provided, prompt for the name of your lambda function
+# if the lambda function does not exist, exit with an error message
+
+# get the argument
+lambda_function_name=$1
+
+# if no argument was provided, prompt for the lambda function name
+if [ -z "$lambda_function_name" ]
+then
+  echo "Enter the name of your lambda function:"
+  read lambda_function_name
+fi
+
+
 start_time=$(date +%s)
 
 while true
 do
-  aws lambda invoke --function-name alarmtest-HelloWorldLambdaFunction-byG45Vo1LM8d outputfile.txt
+  for i in {1..6}
+  do
+    if [ $i -eq 1 ]; then
+      aws lambda invoke --function-name $lambda_function_name outputfile.txt
+    fi
 
-  current_time=$(date +%s)
-  elapsed_time=$(expr $current_time - $start_time)
-  elapsed_minutes=$(expr $elapsed_time / 60)
+    current_time=$(date +%s)
+    elapsed_time=$(expr $current_time - $start_time)
+    elapsed_minutes=$(expr $elapsed_time / 60)
 
-  echo "Elapsed time: $elapsed_minutes minutes"
+    echo "Elapsed time: $elapsed_minutes minutes"
 
-  echo "Checking alarm states..."
-  aws cloudwatch describe-alarms --alarm-name-prefix "alarmtest" --query 'MetricAlarms[*].[AlarmName,AlarmDescription,StateValue]' --output table
+    echo "Checking alarm states..."
+    aws cloudwatch describe-alarms --alarm-name-prefix "alarmtest" --query 'MetricAlarms[*].[AlarmName,AlarmDescription,StateValue,StateUpdatedTimestamp]' --output table
 
-  sleep 60
+    sleep 10
+  done
 done
 
